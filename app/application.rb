db07bc0f81e0d8765dd478579a965c15c6cac943
Aida Abdikulova <abdikulova@gmail.com>
2019-10-07 06:19:54 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,12 @@ class Application
       resp.status = 404
     else
       item = @@items.find(|i| i.name == req.path.split("/items/").last)
-      item.nil? 
+      if item == nil
+        resp.write "Item not found"
+        resp.status = 400
+      else
+        resp.write item.price
+      end
     end
     resp.finish
   end
