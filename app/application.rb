b381521943d20e57569c2483a71a47681c837123
Aida Abdikulova <abdikulova@gmail.com>
2019-10-07 06:11:56 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,13 @@
+class Application
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+    #binding.pry
+    if req.path.match(/items/) == nil
+      resp.write "Route not found"
+      resp.status = 404
+    else
+      
+    end
+  end
+end
