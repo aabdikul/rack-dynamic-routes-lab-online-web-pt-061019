5c0567a9a2103e9244b72bf6054d06d56d752abf
Aida Abdikulova <abdikulova@gmail.com>
2019-10-07 06:24:39 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
-    binding.pry
+    #binding.pry
     if req.path.match(/items/) == nil
       resp.write "Route not found"
       resp.status = 404
