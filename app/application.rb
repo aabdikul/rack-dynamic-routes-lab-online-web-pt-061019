138e09fec6615721696520463dec8d1aae576240
Aida Abdikulova <abdikulova@gmail.com>
2019-10-07 06:18:15 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,4 +1,5 @@
 class Application
+  @@items = []
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
@@ -7,7 +8,7 @@ class Application
       resp.write "Route not found"
       resp.status = 404
     else
-
+      item = @@items.find(|i| i.name == req.path.split("/items/").last)
     end
     resp.finish
   end
